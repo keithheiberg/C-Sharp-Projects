80547b0f82afdae66191453b0cfefce34fa3434a
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-02-06 14:06:54 -0600

    Add files via upload

---
+++ C-sharp_p94/C-sharp_p94/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 5;
+        Console.WriteLine("Count down to zero:");
+        while (n >= 0)
+        {
+            Console.WriteLine(n);
+            n--;
+        }
+        string boredEntry = "";
+        do
+        {
+            Console.WriteLine("Are you bored yet? (y/n)");
+            boredEntry = Console.ReadLine();
+        } while (boredEntry != "y");
+        Console.WriteLine("Me, too. Let's go get a beer.");
+        Console.ReadLine();
+    }
+}
