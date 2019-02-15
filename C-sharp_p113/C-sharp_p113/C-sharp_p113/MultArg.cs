24e3fb3dc22726f05104a233ac6170eddbf1c750
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-02-15 16:09:23 -0600

    Add files via upload

---
+++ C-sharp_p113/C-sharp_p113/C-sharp_p113/MultArg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_p113
+{
+    public class MultArg
+    {
+        public static void DoIt(int firstArg, int secondArg)
+        {
+            try
+            {
+                int number = firstArg * secondArg;
+                Console.WriteLine("Your result: " + number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
