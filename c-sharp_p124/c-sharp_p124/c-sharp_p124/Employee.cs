be02c99a94a424491cdfd19692b6b2b5b671b75c
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-02-19 11:00:08 -0600

    Add files via upload

---
+++ c-sharp_p124/c-sharp_p124/c-sharp_p124/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_p124
+{
+    class Employee : Person
+    {
+        // 1. Create an abstract class called Person with two properties: 
+        // string firstName and string lastName.
+        // 2. Give it the method SayName().
+        // 3. Create another class called Employee and have it inherit from the Person class.
+        // 4. Implement the SayName() method inside of the Employee class.
+        // 5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". 
+        // Call the SayName() method on the object.
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
