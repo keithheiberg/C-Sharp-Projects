90edfd5d14458ce0dcb5db3bac5f9a8733e05967
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-02-25 11:59:16 -0600

    Add files via upload

--- C_sharp_p129/C_sharp_p129/Employee.cs
+++ C_sharp_p129/C_sharp_p129/Employee.cs
@@ -7,11 +7,85 @@ using System.Threading.Tasks;
 namespace C_sharp_p129
 {
     class Employee : Person, IQuittable
+    //public static bool operator ==(TwoDPoint lhs, TwoDPoint rhs)
+    //{
+    //    // Check for null on left side.
+    //    if (Object.ReferenceEquals(lhs, null))
+    //    {
+    //        if (Object.ReferenceEquals(rhs, null))
+    //        {
+    //            // null == null = true.
+    //            return true;
+    //        }
+
+    //        // Only the left side is null.
+    //        return false;
+    //    }
+    //    // Equals handles case of null on right side.
+    //    return lhs.Equals(rhs);
+    //}
+
+    //public static bool operator !=(TwoDPoint lhs, TwoDPoint rhs)
+    //{
+    //    return !(lhs == rhs);
+    //}
+
+    //public static bool operator ==(Box lhs, Box rhs)
+    //{
+    //    bool status = false;
+    //    if (lhs.length == rhs.length && lhs.height == rhs.height
+    //       && lhs.breadth == rhs.breadth)
+    //    {
+
+    //        status = true;
+    //    }
+    //    return status;
+    //}
+    //public static bool operator !=(Box lhs, Box rhs)
+    //{
+    //    bool status = false;
+
+    //    if (lhs.length != rhs.length || lhs.height != rhs.height ||
+    //       lhs.breadth != rhs.breadth)
+    //    {
+
+    //        status = true;
+    //    }
+    //    return status;
+    //}
+
     {
         public int Id { get; set; }
+        bool idEquals = false;
         public void Quit()
         {
             Console.WriteLine("I quit!");
+        }
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            { 
+                bool idEquals = true;
+                return idEquals;
+            }
+            else
+            {
+                bool idEquals = false;
+                return idEquals;
+            }
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                bool idEquals = true;
+                return idEquals;
+            }
+            else
+            {
+                bool idEquals = false;
+                return idEquals;
+            }
         }
     }
 }
--- C_sharp_p129/C_sharp_p129/Program.cs
+++ C_sharp_p129/C_sharp_p129/Program.cs
@@ -12,72 +12,13 @@ namespace C_sharp_p129
         {
             // Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
 
-            //public static bool operator ==(TwoDPoint lhs, TwoDPoint rhs)
-            //{
-            //    // Check for null on left side.
-            //    if (Object.ReferenceEquals(lhs, null))
-            //    {
-            //        if (Object.ReferenceEquals(rhs, null))
-            //        {
-            //            // null == null = true.
-            //            return true;
-            //        }
-
-            //        // Only the left side is null.
-            //        return false;
-            //    }
-            //    // Equals handles case of null on right side.
-            //    return lhs.Equals(rhs);
-            //}
-
-            //public static bool operator !=(TwoDPoint lhs, TwoDPoint rhs)
-            //{
-            //    return !(lhs == rhs);
-            //}
-
-            //public static bool operator ==(Box lhs, Box rhs)
-            //{
-            //    bool status = false;
-            //    if (lhs.length == rhs.length && lhs.height == rhs.height
-            //       && lhs.breadth == rhs.breadth)
-            //    {
-
-            //        status = true;
-            //    }
-            //    return status;
-            //}
-            //public static bool operator !=(Box lhs, Box rhs)
-            //{
-            //    bool status = false;
-
-            //    if (lhs.length != rhs.length || lhs.height != rhs.height ||
-            //       lhs.breadth != rhs.breadth)
-            //    {
-
-            //        status = true;
-            //    }
-            //    return status;
-            //}
-
             Employee employee1 = new Employee();
             Employee employee2 = new Employee();
             employee1.Id = 1;
             employee2.Id = 2;
-            bool idEquals = false;
-        }
-        public static Employee operator ==(Employee employee1, Employee employee2)
-        {
-            if (employee1.Id == employee2.Id)
-                Console.WriteLine("The Ids are NOT equal.");
-            else
-                Console.WriteLine("The Ids are NOT equal.");
-        }
-        public static Employee operator !=(Employee employee1, Employee employee2)
-        {
-            if (employee1.Id == employee2.Id)
-                Console.WriteLine("The Ids are NOT equal.");
-            else
-                Console.WriteLine("The Ids are NOT equal.");
+            bool idEquals = (employee1.Id == employee2.Id);
+            Console.WriteLine("Are employee IDs equal? " + idEquals);
+            Console.ReadLine();
         }
     }
 }
