6d9ae20eb365a9954a0e0e8da19826bfb0643486
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-03-04 13:56:10 -0600

    Add files via upload

---
+++ Logging/Logging/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string card = string.Format("This is a test.");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Keith\logs\log.txt", true))
+            {
+                file.WriteLine(card);
+            }
+            Console.ReadLine();
+        }
+    }
+}
