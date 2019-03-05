ef1f3268c82401078992a77a82b4c95b5724fc2e
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-03-05 10:22:18 -0600

    Add files via upload

---
+++ C_sharp_p154_Logging/Logging/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string numberEntry = Console.ReadLine();
+            string userEntry = string.Format("Your number is: " + numberEntry);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Keith\logs\log.txt", true))
+            {
+                file.WriteLine(userEntry);
+            }
+            string readText = File.ReadAllText(@"C:\Users\Keith\logs\log.txt");
+            Console.WriteLine(readText);
+            Console.ReadLine();
+        }
+    }
+}
