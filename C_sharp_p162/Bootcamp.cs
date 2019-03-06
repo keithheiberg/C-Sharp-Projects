91def6964b957e04fde19b3924ec7f023ea0b267
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-03-06 10:08:26 -0600

    Add files via upload

---
+++ C_sharp_p162/Bootcamp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_p162
+{
+    class Bootcamp
+    {
+        public Bootcamp(string name) : this(name, 100)
+        {
+        }
+        public Bootcamp(string name, int firstCourse)
+        {
+            string Name = name;
+            int Course = firstCourse;
+        }
+        const string courseName = "C# & .Net Framework Course";
+    }
+}
---
+++ C_sharp_p162/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_p162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newBootcamp = new Bootcamp("Software Developer", 100);
+        }
+    }
+}
