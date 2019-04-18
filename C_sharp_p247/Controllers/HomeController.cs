58c5654097e72fa9dd7860e28fda75bc9774175a
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-04-18 10:28:16 -0500

    C# p247
    C# p247

--- C_sharp_p247/Controllers/HomeController.cs
+++ C_sharp_p247/Controllers/HomeController.cs
@@ -111,9 +111,9 @@ namespace C_sharp_p247.Controllers
 
                 db.SignUps.Add(signup);
                 db.SaveChanges();
+                return RedirectToAction("Success");
             }
 
-            return View("Success", signup);
         }
     }
 }
